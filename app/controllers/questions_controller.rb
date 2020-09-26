69db77282442921979642ed0185907cffda88d2c
RD-alpha <65851812+RD-alpha@users.noreply.github.com>
2020-09-26 14:34:05 +0100

    Remove debug statements
    Signed-off-by: RD-alpha <65851812+RD-alpha@users.noreply.github.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,9 @@ class QuestionsController < ApplicationController
     @question_id = @current_q[:id]
     @return_value = "0"
     params.permit(:id)
-    puts @question_id
-    puts params[:id]
-    puts @return_value
     unless @question_id.to_i == params[:id].to_i
       @return_value = "1"
     end
-    puts @return_value
     render plain: @return_value
   end
 end
