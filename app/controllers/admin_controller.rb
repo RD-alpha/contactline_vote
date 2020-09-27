b3a43722ac9dc0780c64cdd49e07568fa2a36a94
RD-alpha <65851812+RD-alpha@users.noreply.github.com>
2020-09-27 17:03:44 +0100

    Add reset button
    Signed-off-by: RD-alpha <65851812+RD-alpha@users.noreply.github.com>

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,26 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with :name => "user", :password => "contactadmin" 
+  http_basic_authenticate_with :name => "user", :password => "contactadmin"
   def index
   end
   def update
-    Question.all.each do |que|
-      que.current = 0
-      que.save
+    if params[:reset]
+      Question.all.each do |que|
+        que.current = 0
+        que.yes_response = 0
+        que.no_response = 0
+        que.save
+      end
+      @cq = Question.find(1)
+      @cq.current = 1
+      @cq.save
+    else
+      Question.all.each do |que|
+        que.current = 0
+        que.save
+      end
+      @cq = Question.find(params[:id_a][:id])
+      @cq.current = 1
+      @cq.save
     end
-    @cq = Question.find(params[:id_a][:id])
-    @cq.current = 1
-    @cq.save
   end
 end
